07ec03993929a46d4de123b32f65860ce6edde3d
Salman <salmanilt1995@gmail.com>
2026-05-12 18:46:41 +0530

    update changes

--- config/mail.php
+++ config/mail.php
@@ -45,9 +45,9 @@ return [
             'port' => env('MAIL_PORT', 2525),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
+//            'timeout' => null,
             'verify_peer' => false,
-            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+//            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
             'stream' => [
                 'tls' => [
                     'verify_peer' => false,
